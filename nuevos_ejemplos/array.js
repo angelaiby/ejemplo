a04d14c3fc5f377a87c31984577b5f7a030f164e
Angela Barriga Yanac <angelaisabelby@gmail.com>
2017-06-30 19:37:08 -0500

    otros

---
+++ nuevos_ejemplos/array.js
@@ -0,0 +1,6 @@
+var valor;
+  valor=prompt('Ingrese un valor:','');
+  valor=parseInt(valor);
+  var resu;
+  resu=Math.pow(valor,3);
+  document.write('El valor '+valor+' elevado a la 3 es '+resu);
